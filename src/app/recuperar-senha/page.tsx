2308075890e7ec14ef4d1af8d801b9fc4bed9851
Davi <davipereiradutra1@gmail.com>
2026-05-30 14:56:19 -0300

    feat: adiciona tela unificada de recuperar e trocar senha

---
+++ src/app/recuperar-senha/page.tsx
@@ -0,0 +1,7 @@
+import { RecoveryPage } from "@/features/auth/RecoveryPage";
+
+// O Next.js precisa obrigatoriamente que a função seja exportada como default
+// e que ela retorne o componente envelopado corretamente.
+export default function RecuperarSenhaPage() {
+  return <RecoveryPage />;
+}
