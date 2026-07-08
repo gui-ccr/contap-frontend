aa4b6edcc93aab28a5c01004dd8090c3bc1c2510
Guilherme Rodrigues <guilhermerodrigues6484@gmail.com>
2026-07-08 15:57:10 -0300

    aumentando o timeout do login

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -17,7 +17,7 @@ if (typeof window !== "undefined") {
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  timeout: 15000,
+  timeout: 45000,
   headers: {
     "Content-Type": "application/json",
   }
